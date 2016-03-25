39b8520319a10d9eebe9606df503a21867cbd22d
Evan Palmer <evjpalmer@gmail.com>
2016-03-25 11:17:52 -0700

    implement models and model unit tests

--- ciet/api/models.py
+++ ciet/api/models.py
@@ -1,19 +1,42 @@
 from django.db import models
+from django.conf import settings
 
 
 class Food(models.Model):
+    """A generic food"""
     name = models.CharField(max_length=50)
-    date_created = models.DateField(auto_now_add=True)
-    date_modified = models.DateField(auto_now=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
 
 
 class DietPlan(models.Model):
-    pass
+    """A user-specific diet plan"""
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    goals = models.CharField(max_length=255, blank=True)
 
 
 class DietPlanFood(models.Model):
-    pass
+    """A food on a specific diet plan, including the food's restrictions"""
+    ALLOWED, NOT_ALLOWED, RESTRICTED, UNASSIGNED = 1, 2, 3, 0
+    FOOD_RESTRICTIONS = (
+        (ALLOWED, 'Allowed'),
+        (NOT_ALLOWED, 'Not Allowed'),
+        (RESTRICTED, 'Restricted'),
+        (UNASSIGNED, 'Unassigned'))
+    food = models.ForeignKey(Food, on_delete=models.CASCADE)
+    plan = models.ForeignKey(DietPlan, on_delete=models.CASCADE)
+    status = models.PositiveSmallIntegerField(
+        choices=FOOD_RESTRICTIONS, default=UNASSIGNED)
+    notes = models.CharField(max_length=255, blank=True)
 
 
 class FoodLog(models.Model):
-    pass
+    """A user's food log associated with a diet plan, including foods eaten"""
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    foods = models.ManyToManyField(DietPlanFood, related_name='logs')
+    date_created = models.DateTimeField(auto_now_add=True)
+    log = models.TextField(blank=True)
--- ciet/api/tests.py
+++ ciet/api/tests.py
@@ -1,17 +1,21 @@
-from datetime import date
+from datetime import datetime
 
 from django.test import TestCase
 from django.core.urlresolvers import resolve
+from django.contrib.auth.models import User
 
 from .views import (food_list, food_detail, plan_list, plan_detail,
     food_list_for_plan)
 from .models import Food, DietPlan, DietPlanFood, FoodLog
 
 
-class APIRouteTestCase(TestCase):
+class APIURLTestCase(TestCase):
     """Test that urls resolve to the correct views"""
     def setUp(self):
         self.prefix = '/api/v1/'
+
+    def tearDown(self):
+        del self.prefix
 
     def test_foods_list_endpoint(self):
         url = self.prefix + 'foods/'
@@ -41,8 +45,54 @@ class APIRouteTestCase(TestCase):
 
 class APIModelTestCase(TestCase):
     """Test that models have the right fields"""
+    def setUp(self):
+        self.user = User.objects.create_user(username="johndoe")
+
     def test_food_model(self):
+        "Test that Food has the correct fields"
         food = Food.objects.create(name='Eggs')
+
         self.assertEqual(food.name, 'Eggs')
-        self.assertIs(type(food.date_created), date)
-        self.assertIs(type(food.date_modified), date)
+        self.assertIs(type(food.date_created), datetime)
+        self.assertIs(type(food.date_modified), datetime)
+
+    def test_diet_plan_model(self):
+        "Test that DietPlan has the correct fields"
+        plan = DietPlan.objects.create(user=self.user)
+
+        self.assertIs(type(plan.date_created), datetime)
+        self.assertIs(type(plan.date_modified), datetime)
+        self.assertIs(type(plan.goals), str)
+        self.assertIs(type(plan.name), str)
+
+    def test_diet_plan_food_model(self):
+        "Test that DietPlanFood has the correct fields and default restrictions"
+        food = Food.objects.create(name='Spam')
+        plan = DietPlan.objects.create(user=self.user)
+        dp_food = DietPlanFood.objects.create(food=food, plan=plan)
+
+        self.assertEqual(dp_food.food.name, 'Spam')
+        self.assertEqual(dp_food.plan, plan)
+        self.assertEqual(dp_food.status, DietPlanFood.UNASSIGNED)
+        self.assertEqual(dp_food.notes, "")
+
+        dp_food.status = DietPlanFood.ALLOWED
+        dp_food.save()
+
+        self.assertEqual(dp_food.status, DietPlanFood.ALLOWED)
+
+    def test_food_log_model(self):
+        """test that FoodLog has the correct fields"""
+        food = Food.objects.create(name='Spam')
+        plan = DietPlan.objects.create(user=self.user)
+        dp_food = DietPlanFood.objects.create(food=food, plan=plan)
+        food_log = FoodLog.objects.create(author=self.user)
+
+        self.assertIsNotNone(food_log.id)
+        self.assertEqual(len(food_log.foods.all()), 0)
+
+        food_log.foods.add(dp_food)
+        self.assertEqual(len(food_log.foods.all()), 1)
+
+        self.assertIs(type(food_log.date_created), datetime)
+        self.assertEqual(food_log.log, "")
