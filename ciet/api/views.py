c8b50f74fb244cc38fdc3e8d0e6cce7bfdb6cb8d
Evan Palmer <evjpalmer@gmail.com>
2016-03-25 17:13:51 -0700

    first iteration of food_list view for GET request

--- ciet/api/views.py
+++ ciet/api/views.py
@@ -1,13 +1,27 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from api.models import Food
+from api.serializers import FoodSerializer
 
 
 def api_index(request):
     return HttpResponse("Hello, you've reached the API index.")
 
 
+@csrf_exempt
+@api_view(['GET'])
 def food_list(request):
-    pass
+    if request.method == 'GET':
+        try:
+            foods = Food.objects.filter(owner=request.user)
+        except TypeError as e:
+            print(e.msg)
+            return HttpResponse(status=403)
+        serializer = FoodSerializer(foods, many=True)
+        return Response(serializer.data)
 
 
 def food_detail(request):
