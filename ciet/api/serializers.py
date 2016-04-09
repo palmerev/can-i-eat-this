42c4821f58c63c77067150fdea95a58dc4cbfeaa
Evan Palmer <evjpalmer@gmail.com>
2016-04-09 10:15:18 -0700

    flesh out serializers

--- ciet/api/serializers.py
+++ ciet/api/serializers.py
@@ -1,9 +1,30 @@
+from django.contrib.auth.models import User
+
 from rest_framework import serializers
 from .models import Food, DietPlan, DietPlanFood
 
 
-class FoodSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username')
 
+
+class FoodSerializer(serializers.ModelSerializer):
     class Meta:
         model = Food
         fields = ('id', 'name', 'date_created', 'date_modified')
+
+
+class DietPlanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DietPlan
+        fields = (
+            'id', 'owner', 'name', 'date_created', 'date_modified', 'goals')
+
+
+class DietPlanFoodSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DietPlanFood
+        fields = ('id', 'food', 'plan', 'status', 'notes')
+        depth = 1
